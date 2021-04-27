143675dabf770f1a31e12a8d05d8ee70ac07c0e1
Brandon <bmacdonald1986@gmail.com>
2021-04-27 20:44:42 +0000

    added assistance requests and teacher data to database

--- 2_queries_joins/1_students_total_assignment_duration.sql
+++ 2_queries_joins/1_students_total_assignment_duration.sql
@@ -1,5 +1,4 @@
-SELECT SUM(duration)
+SELECT SUM(duration) AS total_submissions
 FROM assignment_submissions
-JOIN students
-ON assignment_submissions.student_id = students.id
+JOIN students ON assignment_submissions.student_id = students.id
 WHERE students.name = 'Ibrahim Schimmel';
